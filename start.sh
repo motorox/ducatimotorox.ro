fc31c2acf4a04f0d880c2cc94e21f63b06407ea9
Mitch <mitch_mrc@yahoo.com>
2017-10-02 03:08:10 +0300

    changed a few in the config file and updated the start sh script

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-hugo server -t mtx-hugo-theme --bind 192.168.1.107 --port 8080 --verbose --watch
+hugo server -s ./src/ -t mtx-hugo-theme --bind 192.168.1.108 --port 8080 --verbose --watch
 
