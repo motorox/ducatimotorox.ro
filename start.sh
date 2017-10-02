4d28888c69db7bc78bb73e121305037fa87bc811
Mitch <mitch_mrc@yahoo.com>
2017-10-03 02:07:54 +0300

    corrected the paths when run as a server

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-hugo server -s ./src/ -t mtx-hugo-theme --bind 192.168.1.108 --port 8080 --verbose --watch
+hugo server -s ./src/ --theme mtx-hugo-theme --baseURL 192.168.1.108 --bind 192.168.1.108 --port 8080 --verbose --watch
 
